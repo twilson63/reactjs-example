2a213425b061b81ee036722ffb041aad1f1e3010
Tom Wilson <tom@jackhq.com>
2015-03-12 22:45:50 -0400

    ex2

---
+++ app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+//var TodoTextInput = require('./components/TodoTextInput.react');
+
+React.render(
+  <div>
+    <h1>TodoList</h1>
+    {/* <TodoTextInput id="new-todo" placeholder="What needs to be done?" /> */}
+  </div>,
+  document.getElementById('todoapp')
+);
---
+++ components/TodoTextInput.react.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ENTER_KEY_CODE = 13;
+var TodoTextInput = React.createClass({
+
+  getInitialState: function() {
+    return {
+      value: this.props.value || ''
+    };
+  },
+  render: function() /*object*/ {
+    return (
+      
+    );
+  },
+  _save: function() {},
+  _onChange: function(event) {},
+  _onKeyDown: function(event) {}
+});
+
+module.exports = TodoTextInput;
